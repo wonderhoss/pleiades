7790a7e8bccf3e1bb5d22905bc6fefc7d7c39291
Phil Taprogge <signup@lightweaver.info>
2020-08-06 14:51:14 +0100

    Add cobra

---
+++ cmd/ingest_cmd.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gargath/pleiades/pkg/coordinator"
+	"github.com/gargath/pleiades/pkg/log"
+	"github.com/gargath/pleiades/pkg/publisher/file"
+	"github.com/gargath/pleiades/pkg/publisher/kafka"
+	"github.com/spf13/cobra"
+)
+
+var (
+	cmdIngest = &cobra.Command{
+		Use:   "ingest",
+		Short: "Starts Pleiades ingest server",
+		Long: `ingest starts the ingest server.
+It will begin consuming the WMF stream and publish received events to the configured publisher.`,
+		RunE: startIngest,
+	}
+
+	metricsPort string
+	resume      bool
+	fileOn      bool
+	kafkaOn     bool
+	fileDir     string
+	kafkaBroker string
+	kafkaTopic  string
+)
+
+func init() {
+	cmdIngest.Flags().BoolVarP(&resume, "resume", "r", true, "try to resume from last seen event ID")
+	cmdIngest.Flags().StringVar(&metricsPort, "metricsPort", "9000", "the port to serve Prometheus metrics on")
+	cmdIngest.Flags().BoolVar(&fileOn, "file.enable", false, "enable the filesystem publisher")
+	cmdIngest.Flags().BoolVar(&kafkaOn, "kafka.enable", false, "enable the kafka publisher")
+	cmdIngest.Flags().StringVar(&fileDir, "file.publishDir", "./events", "the directory to publish events to")
+	cmdIngest.Flags().StringVar(&kafkaBroker, "kafka.broker", "localhost:9092", "the kafka broker to connect to")
+	cmdIngest.Flags().StringVar(&kafkaTopic, "kafka.topic", "pleiades-events", "the kafka topic to publish to")
+}
+
+func startIngest(cmd *cobra.Command, args []string) error {
+
+	if verbose && quiet {
+		return fmt.Errorf(" -quiet and -verbose are mutually exclusive")
+	}
+
+	logger = log.MustGetLogger(moduleName)
+	if verbose {
+		log.InitLogLevel(log.VERBOSE)
+	} else if quiet {
+		log.InitLogLevel(log.QUIET)
+	} else {
+		log.InitLogLevel(log.DEFAULT)
+	}
+	logger.Infof("Pleiades %s\n", version())
+
+	c = &coordinator.Coordinator{
+		Resume: resume,
+	}
+
+	if fileOn && kafkaOn {
+		return fmt.Errorf("Can only specify either --file.enable or --kafka.enable")
+
+	} else if !fileOn && !kafkaOn {
+		return fmt.Errorf("No publisher specified")
+	}
+
+	if fileOn {
+		c.File = &file.Opts{
+			Destination: fileDir,
+		}
+	}
+	if kafkaOn {
+		c.Kafka = &kafka.Opts{
+			Broker: kafkaBroker,
+			Topic:  kafkaTopic,
+		}
+	}
+
+	registerShutdownHook()
+
+	initMetrics(metricsPort)
+
+	logger.Info("Starting up...")
+	lastEventID, err := c.Start()
+	if err != nil {
+		return fmt.Errorf("Event consumer exited with error: %v", err)
+	}
+	stopMetrics()
+	logger.Info("Shutdown complete")
+	logger.Infof("Last seen Event ID: %s", lastEventID)
+	return nil
+}
--- cmd/main.go
+++ cmd/main.go
@@ -2,135 +2,49 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	//"log"
 
 	"os"
-	"os/signal"
 
 	"github.com/op/go-logging"
-	flag "github.com/spf13/pflag"
-	"github.com/spf13/viper"
+
+	"github.com/spf13/cobra"
 
 	"github.com/gargath/pleiades/pkg/coordinator"
-	"github.com/gargath/pleiades/pkg/log"
-	"github.com/gargath/pleiades/pkg/publisher/file"
-	"github.com/gargath/pleiades/pkg/publisher/kafka"
 )
 
 const moduleName = "main"
 
 var (
-	c      *coordinator.Coordinator
-	logger *logging.Logger
+	c       *coordinator.Coordinator
+	logger  *logging.Logger
+	verbose bool
+	quiet   bool
 )
 
-func registerShutdownHook() {
-	logger.Debug("Registering shutdown handler")
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt)
-	go func() {
-		<-quit
-		logger.Debug("Shutting down...")
-		c.Stop()
-	}()
-}
-
-func validateFlags() {
-	if viper.GetBool("help") {
-		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
-		fmt.Fprintf(os.Stderr, flag.CommandLine.FlagUsages())
-		os.Exit(0)
-	}
-	if viper.GetBool("verbose") && viper.GetBool("quiet") {
-		fmt.Fprintf(os.Stderr, "ERROR: -quiet and -verbose are mutually exclusive\n")
-		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
-		fmt.Fprintf(os.Stderr, flag.CommandLine.FlagUsages())
-		os.Exit(0)
-	}
-	if !viper.GetBool("kafka.enable") && !viper.GetBool("file.enable") {
-		fmt.Fprintf(os.Stderr, "ERROR: no publisher enabled\n")
-		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
-		fmt.Fprintf(os.Stderr, flag.CommandLine.FlagUsages())
-		os.Exit(0)
-	}
-	if viper.GetBool("kafka.enable") && viper.GetBool("file.enable") {
-		fmt.Fprintf(os.Stderr, "ERROR: only one publisher can be enabled\n")
-		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
-		fmt.Fprintf(os.Stderr, flag.CommandLine.FlagUsages())
-		os.Exit(0)
-	}
-
-}
-
 func main() {
-
-	viper.SetEnvPrefix("PLEIADES")
-	viper.AutomaticEnv()
-
-	//	flag.String("listenAddr", "0.0.0.0:8080", "address to listen on")
-	flag.Bool("help", false, "print this help and exit")
-	flag.String("metricsPort", "9000", "the port to serve Prometheus metrics on")
-	flag.BoolP("verbose", "v", false, "enable verbose output")
-	flag.BoolP("quiet", "q", false, "quiet output - only show ERROR and above")
-	flag.BoolP("resume", "r", true, "try to resume from last seen event ID")
-
-	flag.Bool("file.enable", false, "enable the file publisher")
-	flag.String("file.publishDir", "./events", "the directory to publish events to")
-
-	flag.Bool("kafka.enable", false, "enable the kafka publisher")
-	flag.String("kafka.broker", "localhost:9092", "the kafka broker to connect to")
-	flag.String("kafka.topic", "pleiades-events", "the kafka topic to publish to")
-
-	flag.Parse()
-	viper.BindPFlags(flag.CommandLine)
-
-	validateFlags()
-
-	logger = log.MustGetLogger(moduleName)
-	if viper.GetBool("verbose") {
-		log.InitLogLevel(log.VERBOSE)
-	} else if viper.GetBool("quiet") {
-		log.InitLogLevel(log.QUIET)
-	} else {
-		log.InitLogLevel(log.DEFAULT)
+	var cmdEcho = &cobra.Command{
+		Use:   "echo [string to echo]",
+		Short: "Echo anything to the screen",
+		Long: `echo is for echoing anything back.
+	Echo works a lot like print, except it has a child command.`,
+		Args: cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Echo: " + strings.Join(args, " "))
+		},
 	}
-	logger.Infof("Pleiades %s\n", version())
+	var rootCmd = &cobra.Command{Use: "pleiades"}
 
-	c = &coordinator.Coordinator{
-		Resume: viper.GetBool("resume"),
-	}
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "enable verbose output")
+	rootCmd.PersistentFlags().BoolVarP(&quiet, "quiet", "q", false, "suppress all output except for errors")
 
-	if viper.GetBool("file.enable") && viper.GetBool("kafka.enable") {
-		logger.Error("Can only specify either --file.enable or --kafka.enable")
-		os.Exit(1)
-	} else if !viper.GetBool("file.enable") && !viper.GetBool("kafka.enable") {
-		logger.Error("No publisher specified")
-		os.Exit(1)
-	}
-
-	if viper.GetBool("file.enable") {
-		c.File = &file.Opts{
-			Destination: viper.GetString("file.publishDir"),
-		}
-	}
-	if viper.GetBool("kafka.enable") {
-		c.Kafka = &kafka.Opts{
-			Broker: viper.GetString("kafka.broker"),
-			Topic:  viper.GetString("kafka.topic"),
-		}
-	}
-
-	registerShutdownHook()
-
-	initMetrics(viper.GetString("metricsPort"))
-
-	logger.Info("Starting up...")
-	lastEventID, err := c.Start()
+	rootCmd.AddCommand(cmdEcho)
+	rootCmd.AddCommand(cmdIngest)
+	err := rootCmd.Execute()
 	if err != nil {
-		logger.Errorf("Event consumer exited with error: %v", err)
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(55)
 	}
-	stopMetrics()
-	logger.Info("Shutdown complete")
-	logger.Infof("Last seen Event ID: %s", lastEventID)
 }
